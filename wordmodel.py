304c8c7fbf66b55a3818bd33326001b21ea85308
Nadim-Mahmud <mahmudnadim16@gmail.com>
2020-05-07 14:57:50 +0600

    new fit method added to wordmodel

--- wordmodel.py
+++ wordmodel.py
@@ -14,12 +14,21 @@ Created on Sun May  3 19:21:03 2020
 class WordModel:
     
     connected_words = {}
-    
+    incoming = []
+    response = []
     
     def __init__(self, type):
         self.typ = type    
     
-    def count_associativity(incoming,response):
+    def fit(incoming,response,reverse):
+        WordModel.incoming.extend(incoming)
+        WordModel.response.extend(response)
+        if reverse:
+            pass # temporary
+    
+    def count_associativity():
+        
+        incoming,response = WordModel.incoming, WordModel.response
         
         # iterating throw incoming message
         for i in range(0,len(incoming)):
@@ -30,6 +39,10 @@ class WordModel:
                     for l in range(0,len(response[i])):
                         tmp_response_words = response[i][l].lower().split()
                         for m in tmp_response_words:
+                            # Storing input message into a dictionary then mapping this word to a list
+                            # a list that contains two list 
+                            # first list containing the words asscociated with the incoming message
+                            # second list containig the count of those words
                             if WordModel.connected_words.get(tmp_incoming_words[k],'_nOtFound_') =='_nOtFound_': 
                                 WordModel.connected_words[tmp_incoming_words[k]] = []
                                 WordModel.connected_words[tmp_incoming_words[k]].append([m])
@@ -41,7 +54,7 @@ class WordModel:
                                 else:
                                     index = WordModel.connected_words[tmp_incoming_words[k]][0].index(m)
                                     WordModel.connected_words[tmp_incoming_words[k]][1][index] += 1
-        return WordModel.related_words
+        return WordModel.connected_words
 
 
 
